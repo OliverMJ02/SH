6ead9af3cbe1bc174c89107c558514ae2a450b24
Pontanami <93657710+Pontanami@users.noreply.github.com>
2022-09-24 19:58:40 +0200

    Add files via upload

---
+++ CurryFit/model/MuscleGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurryFit.model
+{ 
+    public enum MuscleGroup
+    {
+        Abs,
+        Biceps,
+        Calves,
+        Chest,
+        Gluteus_Maximus,
+        Gluteus_Medius,
+        Hamstrings,
+        Lats,
+        Obliques,
+        Rhomboids,
+        Shoulders,
+        Trapezoids,
+        Triceps,
+        Teres,
+        Quadriceps
+    }
+  
+}
