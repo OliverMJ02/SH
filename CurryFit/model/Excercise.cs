52ca641fc8f8928b4b47d5217dbff1e854f82893
Pontanami <pontusen@student.chalmers.se>
2022-09-24 12:18:50 +0200

    Start of excelreader

---
+++ CurryFit/model/Excercise.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurryFit.model
+{
+    public class Excersice
+    {
+    }
+}
---
+++ CurryFit/model/datahandling/ExcelReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace CurryFit.model
+{
+   
+    internal class ExcelReader
+    {
+        private static readonly String defaultpath = "../../../resources/";
+
+        static Dictionary<String, Excercise> convertExcelFileToHandledData(String path)
+        {
+            Dictionary<String, Excercise> converted = new Dictionary<String, Excercise>();
+            String line;
+            path = "OvningsDataBas-CurryFit.csv";
+            String file = Path.Combine(defaultpath + path);
+            using (var stream = new StreamReader(file))
+            {
+                stream.ReadLine();
+                while ((line = stream.ReadLine()) != null)
+                {
+                    String[] values = line.Split(';');
+
+                    String name = values[0];
+                    int variant = Int32.Parse(values[5]);
+                    Console.WriteLine(name);
+
+                }
+
+            }
+
+            return converted;
+        }
+
+
+    }
+}
