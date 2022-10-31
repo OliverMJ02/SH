6c00c7cfab9f14313010333cf340812e167d9052
Pontanami <pontusen@student.chalmers.se>
2022-10-31 14:40:12 +0100

    Update Calendar.cs

--- CurryFit/model/Calendar.cs
+++ CurryFit/model/Calendar.cs
@@ -7,11 +7,11 @@ using System.Globalization;
 namespace CurryFit.model
 {
     /// <summary>
-    /// Class for handling the base logic of a calendar
+    /// Class for handling the base logic of a calendar, uses C# DateTime
     /// </summary>
     public class Calendar
     {
-        private int currentDate;
+        private DateTime selectedDate;
         CultureInfo culture = new CultureInfo("en-SE", false);
         String format = "dd MMMM yyyy";
          
@@ -21,10 +21,11 @@ namespace CurryFit.model
         /// </summary>
         /// <param name="date"> the date as a string </param>
         /// <returns></returns
-        public String get_NextDay(String date)
+        public String Get_NextDay(String date)
         {
             var convertedDate = DateTime.Parse(date, culture);
             convertedDate = convertedDate.AddDays(1);
+            selectedDate = convertedDate;
             return convertedDate.ToString(format);
         }
 
@@ -33,10 +34,11 @@ namespace CurryFit.model
         /// </summary>
         /// <param name="date"> the date as a string </param>
         /// <returns></returns>
-        public String get_PreviousDay(String date)
+        public String Get_PreviousDay(String date)
         {
             var convertedDate = DateTime.Parse(date, culture);
             convertedDate = convertedDate.AddDays(-1);
+            selectedDate = convertedDate;
             return convertedDate.ToString(format);
         }
 
@@ -45,10 +47,16 @@ namespace CurryFit.model
         /// </summary>
         /// <param name="date"> the date in DateTime format </param>
         /// <returns> The name of the weekday as a string </returns>
-        public String get_WeekDay(DateTime date)
+        public String Get_WeekDay(DateTime date)
         {
             return date.DayOfWeek.ToString();
         }
+
+        public TimeSpan Get_TimeSpan(DateTime date1, DateTime date2)
+        {
+            return date1.Subtract(date2);
+        }
+
         
     }
 }
