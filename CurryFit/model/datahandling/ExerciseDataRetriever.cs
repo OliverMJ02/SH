6c8cbcc1ea742ec1cc0d926f718ff4e0bf188e08
Pontanami <pontusen@student.chalmers.se>
2022-11-15 20:08:11 +0100

    excelreader update and interface

---
+++ CurryFit/model/datahandling/ExerciseDataRetriever.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurryFit.model.datahandling
+{
+    interface IExerciseDataRetriever
+    {
+        Dictionary<int, Exercise> RetrieveData();
+    }
+}
+
