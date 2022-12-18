1e8cad11b7138153972f0029eec616e689604b4e
Pontanami <pontusen@student.chalmers.se>
2022-12-18 17:40:18 +0100

    DabasAdapter nutrient sorting
    Dabas database apparently has the nutrients not in a fixed order for every product so this is a basic solution to fix it for now

--- CurryFit/model/api/DabasAdapter.cs
+++ CurryFit/model/api/DabasAdapter.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace CurryFit.model.api
 {
+    /// <summary>
+    /// A class for converting a dabas product to a food product
+    /// </summary>
     class DabasAdapter
     {
+        /// <summary>
+        /// Converts the dabas product to a food product
+        /// </summary>
+        /// <param name="dabasProduct">The dabas product to be converted</param>
+        /// <returns>A food product</returns>
         public FoodProduct ConvertToFoodProduct(DabasProduct dabasProduct)
         {
             FoodProduct foodProduct = new FoodProduct();
@@ -15,12 +23,69 @@ namespace CurryFit.model.api
             foodProduct.Brand = dabasProduct.Varumarke.Tillverkare.Namn;
 
             convertNutrients(foodProduct, naringsvarden);
-
             convertContents(dabasProduct, foodProduct);
+
+            foodProduct.Nutrients.Sort((x, y) => x.Name.CompareTo(y.Name));   
+            foodProduct.Nutrients = orderNutrients(foodProduct.Nutrients);
 
             return foodProduct;
         }
 
+        /// <summary>
+        /// Converts the nutrients from the dabas product to the food product
+        /// </summary>
+        /// <param name="foodProduct">The food product to be converted to</param>
+        /// <param name="naringsvarden">The nutrients to be converted</param>
+        private List<FoodProduct.Nutrient> orderNutrients(List<FoodProduct.Nutrient> nutrients)
+        {
+            List<FoodProduct.Nutrient> orderedNutrients = new List<FoodProduct.Nutrient>(nutrients.Count);
+
+            for (int i = 0; i < nutrients.Count; i++)
+            {
+                InsertNutrientsInOrder(nutrients, orderedNutrients, i);
+            }
+
+            return orderedNutrients;
+        }
+        /// <summary>
+        /// Inserts the nutrients in the correct order in the orderedNutrients list
+        /// </summary>
+        /// <param name="nutrients">The list of nutrients to be ordered</param>
+        /// <param name="orderedNutrients">The list of nutrients to be ordered in</param>
+        /// <param name="i">The index of the nutrient to be inserted</param>
+        private void InsertNutrientsInOrder(List<FoodProduct.Nutrient> nutrients, List<FoodProduct.Nutrient> orderedNutrients, int i)
+        {
+            switch (nutrients[i].Name)
+            {
+                case "Energi":
+                    if (nutrients[i].Unit == "kcal")
+                    {
+                        orderedNutrients.Insert(0, nutrients[i]);
+                    }
+                    break;
+                case "Kolhydrat":
+                    orderedNutrients.Insert(1, nutrients[i]);
+                    break;
+                case "Fett":
+                    orderedNutrients.Insert(2, nutrients[i]);
+                    break;
+                case "Protein":
+                    orderedNutrients.Insert(3, nutrients[i]);
+                    break;
+                case "Fiber":
+                    orderedNutrients.Insert(4, nutrients[i]);
+                    break;
+                default:
+                    orderedNutrients.Add(nutrients[i]);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Converts the contents of a DabasProduct to a FoodProduct
+        /// </summary>
+        /// <param name="dabasProduct">The DabasProduct to be converted</param>
+        /// <param name="foodProduct">The FoodProduct to be converted to</param>
         private void convertContents(DabasProduct dabasProduct, FoodProduct foodProduct)
         {
             FoodProduct.Content contents = new FoodProduct.Content();
@@ -30,6 +95,11 @@ namespace CurryFit.model.api
             foodProduct.Contents = contents;
         }
 
+        /// <summary>
+        /// Converts the nutrients of a DabasProduct to a FoodProduct
+        /// </summary>
+        /// <param name="foodProduct">The FoodProduct to be converted to</param>
+        /// <param name="naringsvarden">The nutrients to be converted</param>
         private void convertNutrients(FoodProduct foodProduct, List<DabasProduct.Naringsvarden> naringsvarden)
         {
             List<FoodProduct.Nutrient> nutrients = new List<FoodProduct.Nutrient>(naringsvarden.Count);
