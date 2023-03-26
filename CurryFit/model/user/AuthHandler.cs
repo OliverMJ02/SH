f6f5f8820146dab6287c276391c216d474eb2cdc
Pontanami <pontusen@student.chalmers.se>
2023-03-26 18:28:04 +0200

    Firebase authentication

---
+++ CurryFit/model/user/AuthHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Auth.Requests;
+
+namespace CurryFit.model.user
+{
+    public class AuthHandler
+    {
+        private readonly FirebaseAuthProvider authProvider;
+
+        public AuthHandler(string apikey)
+        {
+        
+        }
+
+
+    }
+}
