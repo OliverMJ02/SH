1e73e743ae54daae47ddcf2427946b0979e7aa6a
OliverMJ02 <79119025+OliverMJ02@users.noreply.github.com>
2023-04-03 22:21:16 +0200

    inställningsalgoritmer vid skapandet

--- CurryFit/view/OpenWorkoutOwner.xaml.cs
+++ CurryFit/view/OpenWorkoutOwner.xaml.cs
@@ -26,7 +26,14 @@ namespace CurryFit.view
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class OpenWorkoutOwner : ContentPage
     {
-        List<(int, string)> list = new List<(int, string)>() { (0, "10"), (1,"20"), (2, "30") };
+        List<(int, string)> RestList = new List<(int, string)>() { (0, "0:10"), (1, "0:20"), (2, "0:30"), (3, "0:45"), (4, "1:00"), (5, "1:15"), (6, "1:30"), (7, "1:45"), (8, "2:00"), (9, "2:30"), (10, "3:00"), (11, "3:30"), (12, "4:00"), (13, "4:30"), (14, "5:00") };
+        List<(int, string)> RepsList = new List<(int, string)>() { (0, "1-3"), (1, "1-5"), (2, "6-8"), (3, "8-12"), (4, "12-16"), (5, "16-20"), (6, "20-30"), (7, "30-40"), (8, "40-50"), (9, "50+") };
+
+
+
+        bool VarRepBool;
+
+
         public OpenWorkoutOwner()
         {
             InitializeComponent();
@@ -38,10 +45,37 @@ namespace CurryFit.view
             var density = mainDisplayInfo.Density;
             var xamarinHeight = deviceHeight / mainDisplayInfo.Density;
             var xamarinWidth = deviceWidth / mainDisplayInfo.Density;
-           
-            
-        
+
+
+            RepToFailureToggle.Toggled += (sender, args) =>
+            {
+
+                if (args.Value == true)
+                {
+                    RepsBox.IsVisible = false;
+                }
+
+                if (args.Value == false)
+                {
+                    RepsBox.IsVisible = true;
+                }
+            };
+
+            VarRepToggle.Toggled += (sender, args) =>
+            {
+                
+                if (args.Value == true)
+                {
+                    VarRepBool = true;
+                }
+
+                if (args.Value == false)
+                {
+                    VarRepBool = false;
+                }
+            };
         }
+
 
         void Handle_ToExercises(object sender, EventArgs e)
         {
@@ -58,19 +92,76 @@ namespace CurryFit.view
         {
             await Navigation.PushAsync(new NewMainPage());
         }
+  
 
-        void Handle_SliderVal(object sender, EventArgs e)
+        void Handle_SliderValNrSets(object sender, EventArgs e)
         {
-            int value = (int)Math.Round(Slider_one.Value);
-            Val_one.Text = value.ToString();
+
+
+
+
+            int value = (int)Math.Round(Slider_NrSets.Value);
+            Val_NrSets.Text = value.ToString();
+
+            if (VarRepBool == true) {
+
+                for (int i = ParentSetInstance.Children.Count - 1; i >= 0; i--)
+                {
+                    ParentSetInstance.Children.RemoveAt(i);
+                }
+
+                // Add new copies
+                for (int i = 0; i < value; i++)
+                {
+                    ParentSetInstance.Children.Add(new StackLayout
+                    {
+                        // Customize the properties of each StackLayout as desired
+                        BackgroundColor = Color.LightGray,
+                        HeightRequest = 50,
+                        Margin = new Thickness(10, 5, 10, 5)
+                    });
+                }
+            }
+
+
+
+            //if (value > 1) {
+            //    for (int i = 0; i < value; i++)
+            //    {
+            //        ParentSetInstance.Children.Add(new StackLayout
+            //        {
+            //            // Customize the properties of each StackLayout as desired
+            //            BackgroundColor = Color.LightGray,
+            //            HeightRequest = 50,
+            //            Margin = new Thickness(10, 5, 10, 5)
+            //        });
+            //    }
+
+
+            //    // Set the content of the page to be the stack layout
+            //    //ParentSetInstance = SetInstance;
+
+            //    SetInstance.Children.Clear();
+
+            //}
+            // Add some views to the stack layout
+        }
+
+
+
+        void Handle_SliderNrReps(object sender, EventArgs e)
+        {
+            int value = (int)Math.Round(Slider_NrReps.Value);
+
+            Val_NrReps.Text = RepsList[value].Item2;
 
         }
 
-        void Handle_SliderValTwo(object sender, EventArgs e)
+        void Handle_SliderValRest(object sender, EventArgs e)
         {
-            int value = (int)Math.Round(Slider_two.Value);
+            int value = (int)Math.Round(Slider_Rest.Value);
 
-            Val_two.Text = list[value].Item2;
+            Val_Rest.Text = RestList[value].Item2;
 
         }
 
