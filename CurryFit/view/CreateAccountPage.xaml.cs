6d67936d43c64db93413f62b1752615d1b815a92
OliverMJ02 <79119025+OliverMJ02@users.noreply.github.com>
2023-04-05 16:51:18 +0200

    uppdat. signin & login

--- CurryFit/view/CreateAccountPage.xaml.cs
+++ CurryFit/view/CreateAccountPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,25 @@ namespace CurryFit.view
         public CreateAccountPage()
         {
             InitializeComponent();
+
+
+
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var deviceHeight = mainDisplayInfo.Height;
+            var deviceWidth = mainDisplayInfo.Width;
+            var density = mainDisplayInfo.Density;
+            var xamarinHeight = deviceHeight / mainDisplayInfo.Density;
+            var xamarinWidth = deviceWidth / mainDisplayInfo.Density;
+
+            boxone.HeightRequest = (xamarinHeight - 600) / 8;
+            boxtwo.HeightRequest = (xamarinHeight - 600) / 8;
+            boxthree.HeightRequest = (xamarinHeight - 600) / 4;
+            //double bone = (xamarinHeight - 600) / 4;
+            //fu.Text = xamarinHeight.ToString();
+            //uf.Text = bone.ToString();
+
+
         }
 
         private async void Handle_ToLogin(object sender, EventArgs e)
--- CurryFit/view/LoginPage.xaml.cs
+++ CurryFit/view/LoginPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,22 @@ namespace CurryFit.view
         public LoginPage()
         {
             InitializeComponent();
+
+
+
+            var mainDisplayInfo = DeviceDisplay.MainDisplayInfo;
+            var deviceHeight = mainDisplayInfo.Height;
+            var deviceWidth = mainDisplayInfo.Width;
+            var density = mainDisplayInfo.Density;
+            var xamarinHeight = deviceHeight / mainDisplayInfo.Density;
+            var xamarinWidth = deviceWidth / mainDisplayInfo.Density;
+
+            boxone.HeightRequest = (xamarinHeight - 600) / 8;
+            boxtwo.HeightRequest = (xamarinHeight - 600) / 8;
+            boxthree.HeightRequest = (xamarinHeight - 600) / 4;
+            //double bone = (xamarinHeight - 600) / 4;
+            //fu.Text = xamarinHeight.ToString();
+            //uf.Text = bone.ToString();
         }
 
         private async void Handle_ToCreateAccount(object sender, EventArgs e)
